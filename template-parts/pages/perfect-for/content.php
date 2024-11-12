9b6d99efd074beb1b20cfe36999dc6580221e319
Luis Azevedo <luismiguelgomes2001@gmail.com>
2024-11-12 11:47:30 -0100

    ADD: Perfect for content v0.1

--- template-parts/pages/perfect-for/content.php
+++ template-parts/pages/perfect-for/content.php
@@ -1,42 +1,46 @@
 <section class="section-perfect-for-content pb-0 -mt-[2px]">
-	<div class="theme-container !px-0 lg:!px-6">
-		<div class="block lg:hidden min-h-20 bg-brown-shade-4 w-full -mt-1"></div>
-		<div class="theme-grid">
-			
+	<div class="theme-container !px-0 lg:mt-16">
+		<div class="block lg:hidden min-h-60 w-full -mt-1"></div>
+		<div class="theme-grid gap-32 md:gap-20 lg:gap-20">
+
 			<?php
-			if ( have_rows( 'child_pages' ) ) :
+			if (have_rows('offers')):
 				$i = 0;
-				while ( have_rows( 'child_pages' ) ) :
+				while (have_rows('offers')):
 					the_row();
 					$fade_class = '';
-					if ( 0 === $i ) :
+					if (0 === $i):
 						$fade_class = 'invisible fade-in--noscroll';
-					else :
+					else:
 						$fade_class = 'invisible fade-in';
 					endif;
 					?>
-					<div class="card-perfect-for grid grid-cols-2 lg:grid-cols-12 col-span-2 lg:col-span-12 gap-x-6 pb-14 lg:pb-0 lg:mb-40 lg:rounded-[20px] <?php echo esc_attr( $fade_class ); ?>">
-						<div class="card-perfect-for-content col-span-2 lg:col-span-5 py-7 lg:pb-14 px-9 lg:px-28 self-end order-2 lg:order-1">
-							<h2 class="text-title-h2 text-brown-shade-1 mb-4 lg:mb-7"><?php the_sub_field( 'title' ); ?></h2>
-							<h3 class="text-title-h3 text-brown-shade-1 mb-4 lg:mb-7"><?php the_sub_field( 'subtitle' ); ?></h3>
-							<div class="text-body text-brown-shade-1 mb-12"><?php the_sub_field( 'description' ); ?></div>
+					<div
+						class="card-perfect-for bg-slate-500 grid grid-cols-2 lg:grid-cols-12 col-span-2 lg:col-span-12 gap-x-6 pb-14 lg:pb-0 <?php echo esc_attr($fade_class); ?>">
+						<div
+							class="card-perfect-for-content col-span-2 lg:col-span-5 py-7 lg:pb-14 px-9 lg:px-28 self-end order-2 <?php echo ($i === 1) ? 'lg:order-2' : 'lg:order-1'; ?>">
+							<h2 class="text-title-h2 mb-4 lg:mb-7"><?php the_sub_field('title'); ?></h2>
+							<h3 class="text-title-h3 mb-4 lg:mb-7"><?php the_sub_field('subtitle'); ?></h3>
+							<div class="text-body mb-12"><?php the_sub_field('description'); ?></div>
 							<?php
-							$ilink = get_sub_field( 'link' );
-							if ( $ilink ) :
-								$link_url    = $ilink['url'];
-								$link_title  = $ilink['title'];
+							$ilink = get_sub_field('link');
+							if ($ilink):
+								$link_url = $ilink['url'];
+								$link_title = $ilink['title'];
 								$link_target = $ilink['target'] ? $ilink['target'] : '_self';
 								?>
-								<a class="btn-internal btn-internal--shade-transperant !text-brown-shade-1" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+								<a class="btn-internal btn-internal--shade-transperant font-light" href="<?php echo esc_url($link_url); ?>"
+									target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
 								<?php
 							endif;
 							?>
 						</div>
-						<div class="card-perfect-for-image col-start-1 lg:col-start-6 col-span-2 lg:col-span-6 relative order-1 lg:order-2">
+						<div
+							class="card-perfect-for-image col-start-1 col-span-2 lg:col-span-7 relative order-1 <?php echo ($i === 1) ? 'lg:order-1' : 'lg:order-2'; ?>">
 							<?php
-							$iimage = get_sub_field( 'image' );
-							if ( $iimage ) :
-								echo wp_get_attachment_image( $iimage, 'full', false, array( 'class' => 'relative -mt-12 lg:mt-0 lg:-top-14 w-full object-fit px-6 lg:px-0  rounded-[20px] min-h-[217px] lg:min-h-[432px]' ) );
+							$offer_image = get_sub_field('image');
+							if ($offer_image):
+								echo wp_get_attachment_image($offer_image, 'full', false, array('class' => 'relative lg:mt-0 lg:-top-2 px-6 lg:px-0 min-h-[217px] lg:min-h-[432px]'));
 							endif;
 							?>
 						</div>
@@ -52,8 +56,7 @@
 <section class="section-page-teasers py-8 lg:py-32 xl:pt-36">
 	<div class="theme-container">
 		<div class="theme-grid">
-			<?php get_template_part( 'template-parts/components/teaser-default' ); ?>
+			<?php get_template_part('template-parts/components/teaser-default'); ?>
 		</div>
 	</div>
 </section>
-
