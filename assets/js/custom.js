e439d0253c44748af62df64f8124b41382577fe3
Daniel-Digid <daniel@dig.id>
2024-11-12 15:46:47 +0000

    d

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const scrollLink = document.getElementById('scroll-down-link');
+    if (scrollLink) {
+        scrollLink.addEventListener('click', function() {
+            window.scrollBy({
+                top: window.innerHeight,
+                behavior: 'smooth'
+            });
+        });
+    }
+
+});
+
+
 let header = $('#header-main');
 let menuApp = $('.menu-app');
 let lastScroll = 0;
