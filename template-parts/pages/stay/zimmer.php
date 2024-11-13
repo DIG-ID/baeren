e7de903068f2c404fd8eec668f86ed8140e59a20
Daniel-Digid <daniel@dig.id>
2024-11-13 13:45:34 +0000

    d

--- template-parts/pages/stay/zimmer.php
+++ template-parts/pages/stay/zimmer.php
@@ -1,33 +1,67 @@
-<section class="section-zimmer-teaser bg-brown-shade-4 text-brown-shade-1 pb-20 lg:pb-40">
+<section class="section-zimmer-teaser bg-white pt-36 pb-20 lg:pb-40">
 	<div class="theme-container">
-		<h2 class="text-title text-center mb-6 lg:mb-20 invisible fade-in--noscroll"><?php the_field( 'zimmer_title' ); ?></h2>
-		<div class="theme-grid">
-			<div class="col-span-2 lg:col-span-4 lg:col-start-2 order-2 lg:order-1 invisible fade-in--noscroll">
-				<h3 class="text-title-h3 mb-5 "><?php the_field( 'zimmer_subtitle_01' ); ?></h3>
-				<p class="text-body mb-16"><?php the_field( 'zimmer_description_01' ); ?></p>
-				<h3 class="text-title-h3 mb-5"><?php the_field( 'zimmer_subtitle_02' ); ?></h3>
-				<p class="text-body mb-16"><?php the_field( 'zimmer_description_02' ); ?></p>
-				<?php
-				$zlink = get_field( 'zimmer_link' );
-				if ( $zlink ) :
-					$link_url    = $zlink['url'];
-					$link_title  = $zlink['title'];
-					$link_target = $zlink['target'] ? $zlink['target'] : '_self';
-					?>
-					<a class="btn-internal btn-internal--shade-5" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
-					<?php
-				endif;
-				?>
-			</div>
-			<div class="col-span-2 lg:col-start-7 lg:col-span-5 order-1 lg:order-2 invisible fade-in--noscroll">
-				<?php
-				$zimage = get_field( 'zimmer_image' );
-				if ( $zimage ) :
-					echo wp_get_attachment_image( $zimage, 'full', false, array( 'class' => 'w-full object-cover rounded-[20px] mb-10 lg:mb-0' ) );
-				endif;
-				?>
-			</div>
-		</div>
+		<ul class="zimmer__list">
+		<?php
+		if( have_rows('zimmer') ):
+			while( have_rows('zimmer') ) : the_row(); 
+		?>
+			<li class="zimmer__item">
+    			<div class="zimmer__item--body section-container">
+        			<div class="zimmer__item--wrapper">
+            			<h2 class="zimmer__item--title section-title">
+                			<?php the_sub_field( 'title' ); ?>
+            			</h2>
+            			<div class="zimmer__item--info">
+                			<ul>
+								<li>
+									2 rooms
+								</li>
+								<li>
+									THE HOUSE AREA: 80 SQ.M
+								</li>
+								<li>
+									200$ / PER NIGHT
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="zimmer__item--body section-container _clone">
+					<div class="zimmer__item--bg">
+						<picture>
+						<?php
+						$zimmer_image = get_sub_field( 'background' );
+						if ( $zimmer_image ) :
+							echo wp_get_attachment_image( $zimmer_image, 'full', false, array( 'class' => 'w-full object-cover' ) );
+						endif;
+						?>
+						</picture>
+					</div>
+		
+					<div class="zimmer__item--wrapper">
+						<h2 class="zimmer__item--title section-title">
+							<?php the_sub_field( 'title' ); ?>
+						</h2>
+						<div class="zimmer__item--info">
+							<ul>
+								<li>
+									2 rooms
+								</li>
+								<li>
+									THE HOUSE AREA: 80 SQ.M
+								</li>
+								<li>
+									200$ / PER NIGHT
+								</li>
+							</ul>
+						</div>
+					</div>
+    			</div>
+			</li>
+		<?php
+			endwhile;
+		endif;
+		?>
+		</ul>
 	</div>
 </section>
-<hr class="wave-separator border-0" />
