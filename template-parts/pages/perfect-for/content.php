b75769dac4b33b3adf080377da5b7af73f6eeaa7
Luis Azevedo <luismiguelgomes2001@gmail.com>
2024-11-13 11:32:55 -0100

    ADD: Perfect holiday page

--- template-parts/pages/perfect-for/content.php
+++ template-parts/pages/perfect-for/content.php
@@ -1,29 +1,34 @@
-<section class="section-perfect-for-content pb-0 -mt-[2px]">
-	<div class="theme-container !px-0 lg:mt-16">
-		<div class="block lg:hidden min-h-60 w-full -mt-1"></div>
-		<div class="theme-grid gap-32 md:gap-20 lg:gap-20">
+<section class="section-perfect-for-content pb-0 flex flex-col gap-40 my-20">
+	<?php
+	if (have_rows('offers')):
+		$i = 0;
+		while (have_rows('offers')):
+			the_row();
+			$fade_class = '';
+			if (0 === $i):
+				$fade_class = 'invisible fade-in--noscroll';
+			else:
+				$fade_class = 'invisible fade-in';
+			endif;
+			?>
 
-			<?php
-			if (have_rows('offers')):
-				$i = 0;
-				while (have_rows('offers')):
-					the_row();
-					$fade_class = '';
-					if (0 === $i):
-						$fade_class = 'invisible fade-in--noscroll';
-					else:
-						$fade_class = 'invisible fade-in';
-					endif;
-					?>
+			<?php if ($i === 1): ?>
+				<div class="relative bg-brown-shade-1 py-10">
+					<span class="diamond--top"></span>
+					<span class="diamond--bottom"></span>
+				<?php endif; ?>
+
+				<div class="theme-container !px-0 mx-auto">
+					<div class="block lg:hidden min-h-60 -mt-1"></div>
+					<div class="theme-grid"></div>
 					<div
-						class="card-perfect-for grid grid-cols-2 lg:grid-cols-12 col-span-2 lg:col-span-12 gap-x-6 pb-14 lg:pb-0 <?php echo esc_attr($fade_class); ?>">
+						class="relative card-perfect-for grid grid-cols-2 lg:grid-cols-12 col-span-2 lg:col-span-12 gap-x-6 pb-14 lg:pb-0 <?php echo esc_attr($fade_class); ?>">
 						<div
 							class="card-perfect-for-content col-span-2 lg:col-span-5 py-7 my-auto lg:pb-14 px-9 lg:px-28 self-end order-2 <?php echo ($i === 1) ? 'lg:order-2' : 'lg:order-1'; ?>">
-							<span class="diamond-top"></span>
+
 							<h2 class="text-title-h2 mb-4 lg:mb-7"><?php the_sub_field('title'); ?></h2>
 							<h3 class="text-title-h3 mb-4 lg:mb-7"><?php the_sub_field('subtitle'); ?></h3>
 							<div class="text-body mb-12"><?php the_sub_field('description'); ?></div>
-							<span class="diamond-bottom"></span>
 
 							<?php
 							$ilink = get_sub_field('link');
@@ -36,8 +41,13 @@
 									target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
 								<?php
 							endif;
-							?>
+							if ($i !== 1): ?>
+								<div class="diamond-container ">
+									<span class="diamond--transparent"></span>
+								</div>
+							<?php endif; ?>
 						</div>
+
 						<div
 							class="card-perfect-for-image col-start-1 col-span-2 lg:col-span-7 relative order-1 <?php echo ($i === 1) ? 'lg:order-1' : 'lg:order-2'; ?>">
 							<?php
@@ -48,15 +58,21 @@
 							?>
 						</div>
 					</div>
-					<?php
-					$i++;
-				endwhile;
-			endif;
-			?>
-		</div>
-	</div>
+				</div>
+
+				<?php if ($i === 1): ?>
+				</div>
+			<?php endif; ?>
+
+
+			<?php
+			$i++;
+		endwhile;
+	endif;
+	?>
+
 </section>
-<section class="section-page-teasers py-8 lg:py-32 xl:pt-36">
+<section class="section-page-teasers py-8 lg:py-32 xl:pt-36 bg-brown-shade-1">
 	<div class="theme-container">
 		<div class="theme-grid">
 			<?php get_template_part('template-parts/components/teaser-default'); ?>
