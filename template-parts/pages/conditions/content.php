13d99f0ea63db0f75aa0639e78677a8a46e19a02
Luis Azevedo <luismiguelgomes2001@gmail.com>
2024-11-26 12:56:06 -0100

    FIX: Conditions page

--- template-parts/pages/conditions/content.php
+++ template-parts/pages/conditions/content.php
@@ -1,13 +1,28 @@
-<section class="section-306-touur-content p-16 lg:p-28">
+<section class="section-conditions-content p-16 lg:p-28">
+<?php if (have_rows('conditions_sections')): ?>
     <div class="theme-container">
         <?php
-        $conditions = get_field('conditions_text');
+        while (have_rows('conditions_sections')):
+            the_row();
+            $conditionTitle = get_sub_field('title');
+            $conditionSubTitle = get_sub_field('subtitle');
+            $conditionDescription = get_sub_field('description');
         ?>
-        <div
-            class="lg:mr-48 invisible fade-in--noscroll">
-            <div class="text-body">
-                <?php echo wpautop($conditions); ?>
+            <div class="lg:mr-48 invisible fade-in--noscroll pb-8">
+                <?php if ($conditionTitle): ?>
+                    <h3 class="text-title-h3 pb-4"><?php echo esc_html($conditionTitle); ?></h3>
+                <?php endif; ?>
+                <?php if ($conditionSubTitle): ?>
+                    <h4 class="font-bold text-sm"><?php echo esc_html($conditionSubTitle); ?></h4>
+                <?php endif; ?>
+                <?php if ($conditionDescription): ?>
+                    <div class="text-body">
+                        <?php echo wpautop($conditionDescription); ?>
+                    </div>
+                <?php endif; ?>
             </div>
-        </div>
+        <?php endwhile; ?>
     </div>
+<?php endif; ?>
+
 </section>
