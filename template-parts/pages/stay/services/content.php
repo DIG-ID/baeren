a7f8fa734cf002afb028136daa0387cbaeb55c95
Luis Azevedo <luismiguelgomes2001@gmail.com>
2024-12-02 15:00:37 -0100

    ADD: Services // FIX: Perfect-for secondary sass file names

--- template-parts/pages/stay/services/content.php
+++ template-parts/pages/stay/services/content.php
@@ -1,34 +1,66 @@
-<section class="section-services-content bg-brown-shade-2 py-8 lg:py-16">
-	<div class="theme-container">
-		<div class="theme-grid invisible fade-in--noscroll">
+<section class="section-services-content pb-0 flex flex-col gap-52 lg:gap-40 my-20">
+	<?php if (have_rows('services')): ?>
+		<?php $i = 1; ?>
+		<?php while (have_rows('services')):
+			the_row(); ?>
+
 			<?php
-			if ( have_rows( 'services' ) ) :
-				while ( have_rows( 'services' ) ) :
-					the_row();
-					?>
-						<div class="card-service col-span-2 lg:col-span-6 bg-white py-12 lg:py-20 px-9 lg:px-28 rounded-[20px] mb-6">
-							<h2 class="text-title-h3 mb-4 lg:mb-7"><?php the_sub_field( 'title' ); ?></h2>
-							<p class="text-body"><?php the_sub_field( 'description' ); ?></p>
-						</div>
-					<?php
-				endwhile;
-			endif;
-			$info = get_field( 'info' );
-			if ( $info ) :
-				?>
-				<div class="card-service col-span-2 lg:col-span-6 bg-white py-12 lg:py-20 px-9 lg:px-28 rounded-[20px] mb-6 self-start">
-					<div class="flex flex-col lg:flex-row">
-						<div class="mb-2 lg:mb-0 lg:mr-3">
-							<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-								<path d="M9 15H11V9H9V15ZM10 7C10.2833 7 10.521 6.904 10.713 6.712C10.905 6.52 11.0007 6.28267 11 6C11 5.71667 10.904 5.47933 10.712 5.288C10.52 5.09667 10.2827 5.00067 10 5C9.71667 5 9.47933 5.096 9.288 5.288C9.09667 5.48 9.00067 5.71733 9 6C9 6.28333 9.096 6.521 9.288 6.713C9.48 6.905 9.71733 7.00067 10 7ZM10 20C8.61667 20 7.31667 19.7373 6.1 19.212C4.88333 18.6867 3.825 17.9743 2.925 17.075C2.025 16.175 1.31267 15.1167 0.788 13.9C0.263333 12.6833 0.000666667 11.3833 0 10C0 8.61667 0.262667 7.31667 0.788 6.1C1.31333 4.88333 2.02567 3.825 2.925 2.925C3.825 2.025 4.88333 1.31267 6.1 0.788C7.31667 0.263333 8.61667 0.000666667 10 0C11.3833 0 12.6833 0.262667 13.9 0.788C15.1167 1.31333 16.175 2.02567 17.075 2.925C17.975 3.825 18.6877 4.88333 19.213 6.1C19.7383 7.31667 20.0007 8.61667 20 10C20 11.3833 19.7373 12.6833 19.212 13.9C18.6867 15.1167 17.9743 16.175 17.075 17.075C16.175 17.975 15.1167 18.6877 13.9 19.213C12.6833 19.7383 11.3833 20.0007 10 20ZM10 18C12.2333 18 14.125 17.225 15.675 15.675C17.225 14.125 18 12.2333 18 10C18 7.76667 17.225 5.875 15.675 4.325C14.125 2.775 12.2333 2 10 2C7.76667 2 5.875 2.775 4.325 4.325C2.775 5.875 2 7.76667 2 10C2 12.2333 2.775 14.125 4.325 15.675C5.875 17.225 7.76667 18 10 18Z" fill="#0E324A"/>
-							</svg>
-						</div>
-						<p class="text-body"><?php the_field( 'info_description' ); ?></p>
-					</div>
-				</div>
-			<?php
-			endif;
+			$is_even = $i % 2 === 0;
+			$fade_class = ($i === 1) ? 'invisible fade-in--noscroll' : 'invisible fade-in';
+			$order_image_class = $is_even ? ' lg:order-1 lg:after:left-[0.62rem] lg:after:right-[-2rem] lg:mr-[-6rem] mt-[-6rem] lg:mt-[3rem]' : ' lg:order-2 lg:after:left-[-2rem] lg:after:right-[0.62rem] lg:ml-[-6rem]';
 			?>
-		</div>
-	</div>
+
+			<?php if ($is_even): ?>
+				<div class="relative bg-brown-shade-1 lg:py-20 invisible fade-in">
+				<?php endif; ?>
+
+				<div class="theme-container !px-0 mx-auto">
+					<div class="theme-grid"></div>
+					<div
+						class="relative card-services items-center grid grid-cols-2 mx-8 lg:grid-cols-12 col-span-2 lg:col-span-12 gap-x-6 lg:pb-0 lg:mx-16 <?php echo esc_attr($fade_class); ?>">
+
+						<div
+							class="card-services-content col-span-2 py-28 px-4 lg:px-12 lg:col-span-6 order-2 <?php echo $is_even ? 'lg:order-2 lg:pl-40 lg:py-48' : 'lg:order-1 lg:pr-40 lg:py-44'; ?>">
+							<h2 class="text-title-h2 lg:text-2xl text-center lg:text-left mb-10 mx-auto max-w-[355px]"><?php the_sub_field('title'); ?></h2>
+							<h3 class="text-title-h3 lg:text-base mb-4 lg:mb-7"><strong><?php the_sub_field('subtitle'); ?></strong></h3>
+
+							<?php
+							$list = get_sub_field('list');
+							if ($list):
+								echo '<ul class="list-none">';
+								foreach ($list as $list_item):
+									echo '<li class="mt-6" ><span class="inline-block mr-2 w-2 h-2 rotate-45 bg-brown-shade-2"></span> ' . esc_html($list_item['text']) . '</li>';
+								endforeach;
+								echo '</ul>';
+							endif;
+							?>
+
+							<div class="text-body mb-12 text-center lg:text-left mx-auto max-w-[355px]"><?php the_sub_field('description'); ?></div>
+						</div>
+
+						<div
+							class="card-services-image col-span-2 lg:col-span-6 relative bg-white lg:my-12 order-1 <?php echo $order_image_class ?>">
+							<div
+								class="absolute lg:w-8 lg:h-16 w-16 h-8 flex justify-center items-center lg:bg-brown-shade-1 z-10 bottom-[-2rem] lg:bottom-1/2 left-1/2 translate-y-1/2 -translate-x-1/2 <?php echo $is_even ? 'lg:!right-[-2rem] lg:left-full lg:translate-x-1/2 ' : 'lg:!left-[-2rem] lg:-translate-x-1/2 bg-white ' ?>">
+								<span class="block h-4 w-4 rotate-45 <?php echo $is_even ? 'diamond--light-brown' : 'diamond--white' ?> lg:!bg-brown-shade-1 "></span>
+							</div>
+
+							<?php
+							$offer_image = get_sub_field('image');
+							if ($offer_image):
+								echo wp_get_attachment_image($offer_image, 'full', false, array('class' => 'w-full object-cover'));
+							endif;
+							?>
+						</div>
+					</div>
+
+				</div>
+
+				<?php if ($i % 2 === 0): ?>
+				</div>
+			<?php endif; ?>
+
+			<?php $i++; ?>
+		<?php endwhile; ?>
+	<?php endif; ?>
 </section>
