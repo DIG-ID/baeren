dc9c4a94b3230c7716e3bb79cf20d68b53e1efb9
Luis Azevedo <luismiguelgomes2001@gmail.com>
2024-12-04 10:09:03 -0100

    FIX: Hide about history, team first and header color on perfect for child pages

--- page-templates/page-about.php
+++ page-templates/page-about.php
@@ -9,10 +9,10 @@ if ( have_posts() ) :
 		the_post();
 		do_action( 'before_main_content' );
 			get_template_part( 'template-parts/pages/page-header' );
-			get_template_part( 'template-parts/pages/about/philosophie' );
-			get_template_part( 'template-parts/pages/about/history' );
-			get_template_part( 'template-parts/pages/about/architecture' );
 			get_template_part( 'template-parts/pages/about/team' );
+			get_template_part( 'template-parts/pages/about/philosophie' );
+/* 			get_template_part( 'template-parts/pages/about/history' );
+ */			get_template_part( 'template-parts/pages/about/architecture' );
 		do_action( 'after_main_content' );
 	endwhile;
 endif;
