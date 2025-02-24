4932f4506d1b06edd41ac55673c3f52de6cbe3ea
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2025-02-24 16:04:20 +0000

    changing margin top on the button

--- template-parts/pages/perfect-for/cyclists/extras.php
+++ template-parts/pages/perfect-for/cyclists/extras.php
@@ -19,7 +19,7 @@
 								$link_target = $link['target'] ? $link['target'] : '_self';
 								?>
 								<a href="<?php echo esc_url($link_url); ?>"
-									class="btn btn--secondary mb-4 mt-16 lg:mb-0" target="_blank">
+									class="btn btn--secondary mb-4 mt-6 lg:mb-0" target="_blank">
 									<h3 class="text-body text-brown-shade-4 hover:transform-scale">
 										<?php echo esc_html($link_title); ?></h3>
 									<svg class="" width="19" height="10" viewBox="0 0 19 10" fill="none"
